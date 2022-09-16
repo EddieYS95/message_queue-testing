c94478755d8ce69ad9b06e7fb27559277f29ef73
JeongYS <kunks8271@gmail.com>
2022-09-16 10:47:39 +0900

    feat: init project

---
+++ publisher-spring/src/main/java/com/nerdllgeek/publisherspring/PublisherSpringApplication.java
@@ -0,0 +1,13 @@
+package com.nerdllgeek.publisherspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublisherSpringApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PublisherSpringApplication.class, args);
+  }
+
+}
---
+++ publisher-spring/src/test/java/com/nerdllgeek/publisherspring/PublisherSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nerdllgeek.publisherspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublisherSpringApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
